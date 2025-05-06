0aed3ef17465cfdd441fff4099e1c6bc89dfb8a0
Priyanka H <123575447+Priyankah2004@users.noreply.github.com>
2025-05-06 09:31:54 +0530

    Update Program_01.java

--- src/com/Test/Program_01.java
+++ src/com/Test/Program_01.java
@@ -3,7 +3,12 @@ package com.Test;
 public class Program_01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int x=scan.nextInt();
+		int s=x*x;
+		System.out.println("the square of the number"+x+"is"+s);
 
 	}
 
